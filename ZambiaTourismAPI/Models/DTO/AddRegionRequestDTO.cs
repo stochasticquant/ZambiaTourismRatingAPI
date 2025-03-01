257896eb1ad7e76edc705107a45c3806e77d2bad
charles <sambocharles@outlook.com>
2025-03-01 21:06:50 +0100

    added CRUD using DTOs

---
+++ ZambiaTourismAPI/Models/DTO/AddRegionRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ZambiaTourismAPI.Models.DTO
+{
+    public class AddRegionRequestDTO
+    {
+        public string RegionCode { get; set; }
+        public string RegionName { get; set; }
+        public string RegionDescription { get; set; }
+        public string? RegionImage { get; set; }
+        public string? RegionURL { get; set; }
+    }
+}
---
+++ ZambiaTourismAPI/Models/DTO/UpdateRegionRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ZambiaTourismAPI.Models.DTO
+{
+    // This DTO should only have properties allowed to be updated as per business requirements
+    public class UpdateRegionRequestDTO
+    {
+        public string RegionCode { get; set; }
+        public string RegionName { get; set; }
+        public string RegionDescription { get; set; }
+        public string? RegionImage { get; set; }
+        public string? RegionURL { get; set; }
+    }
+}
